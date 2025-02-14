a3be70201a4378030cb5b162f8646d73dcb452ab
Nishtha Sharma <ns7558318@gmail.com>
2025-02-14 14:42:12 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -19,7 +19,7 @@ int main()
             printf("%.0f\n",x/y);
             }
             else{
-                printf("Error");
+                printf("error");
             }
             break;
         default:
